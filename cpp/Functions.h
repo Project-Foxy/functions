02fc2d78e03138e680eeeca6e2d90257ac639367
StioStudio <simon.s.johanssen@gmail.com>
2022-08-30 13:29:38 +0200

    save

--- cpp/Functions.h
+++ cpp/Functions.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <iostream>
 #include <functional>
+#include <unistd.h>
+
 
 void say(char* message...);
 
 void stop();
 
 void repeat(int times, std::function<void()> func);
+
+void forever(std::function<void()> func, int fps = 60);
